e0b1226d5ebc482fd7e0defb4af345649e7d890b
AssafSalinas <105939864+AssafSalinas@users.noreply.github.com>
2022-05-26 12:28:09 -0500

    Add files via upload

--- Bitacora2/src/Conexion.java
+++ Bitacora2/src/Conexion.java
@@ -6,42 +6,38 @@ public class Conexion {
 	private static final String url = "jdbc:mysql://localhost:3306/bitacora2";
 	private static final String root = "root";
 	private static final String controlador = "com.mysql.cj.jdbc.Driver";
-    private static final String password = "";
-    
-    static {
-    	try {
+	private static final String password = "";
+	private static final String greet = "true";
+
+	static {
+		try {
 			Class.forName(controlador);
-			System.out.println("El controlador se cargo exitosamente"); 
+			System.out.println("El controlador se cargo exitosamente");
 
-		} 
-    	
-    	catch (ClassNotFoundException e) {
-			System.out.println("Error al cargar el controlador"); 
-    		e.printStackTrace();
 		}
-    }
-    
-   public Connection conexion() {
-	   
-	     Connection conexion = null;
-	     
-	   try {
-		conexion = DriverManager.getConnection(url,root,password);
-		System.out.println("Conexion exitosa con la base de datos"); 
 
-		
-	} catch (SQLException e) {
-		System.out.println("Consulta no exitosa");	
-		e.printStackTrace();
-			
+		catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el controlador");
+			e.printStackTrace();
+		}
 	}
-	return conexion;
-	
+
+	public Connection Conectar() {
+
+		Connection Conectar = null;
+
+		try {
+			Conectar = DriverManager.getConnection(url, root, password);
+			System.out.println();
+			System.out.println(greet);
+
+		} catch (SQLException e) {
+			System.out.println("Consulta no exitosa");
+			e.printStackTrace();
+
+		}
+		return Conectar;
+
+	}
+
 }
-
- 
-
-}
-
-
-
